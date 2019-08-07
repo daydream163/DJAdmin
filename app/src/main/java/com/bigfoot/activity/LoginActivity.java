0490bbe140cd20c358f9bd0ede2f875575ec37a4
songjunpeng <songjunpeng@greenlandfinancial.com>
2019-08-07 18:26:33 +0800

    第一版，先弄个开发框架上去，基本的功能都有了，后面在这个框架上改。

---
+++ app/src/main/java/com/bigfoot/activity/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.bigfoot.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.bigfoot.R;
+import com.bigfoot.ui.swipebacklayout.SwipeBackActivity;
+
+
+/**
+ * Created by tiansj on 15/7/31.
+ */
+public class LoginActivity extends SwipeBackActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.btnClose).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
