2b634b92d5d06cd739042ec009338b693a07a175
songjunpeng <songjunpeng@greenlandfinancial.com>
2019-08-13 18:04:23 +0800

    设置。

--- app/src/main/java/com/bigfoot/activity/SettingActivity.java
+++ app/src/main/java/com/bigfoot/activity/SettingActivity.java
@@ -1,5 +1,14 @@
 package com.bigfoot.activity;
 
+import android.os.Bundle;
+
+import com.bigfoot.R;
+
 public class SettingActivity extends BaseFragmentActivity {
-    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
 }
