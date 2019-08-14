b5316538867b535ee3565c8749ea0d3e1e54c85f
songjunpeng <songjunpeng@greenlandfinancial.com>
2019-08-14 14:51:43 +0800

    添加商品界面。

---
+++ app/src/main/java/com/bigfoot/activity/SingleProductActiviy.java
@@ -0,0 +1,13 @@
+package com.bigfoot.activity;
+
+import android.os.Bundle;
+
+import com.bigfoot.R;
+
+public class SingleProductActiviy extends BaseFragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+    }
+}
